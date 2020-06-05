963673eb4c877f958c0b513b84f2b1b47d0ac102
Egor Ukolov <ukolovegor@yandex.ru>
2020-06-05 19:27:47 +0700

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Egor Ukolov on 05.06.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
