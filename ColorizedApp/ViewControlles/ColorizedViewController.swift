8be200c564c376e81506ce68b005f2634304bdb2
Egor Ukolov <ukolovegor@yandex.ru>
2020-06-06 11:16:58 +0700

    Добавление трех TextFields к red, green и blue индикаторам. Создание их outlets.

--- ColorizedApp/ViewControlles/ColorizedViewController.swift
+++ ColorizedApp/ViewControlles/ColorizedViewController.swift
@@ -10,60 +10,72 @@ import UIKit
 
 class ColorizedViewController: UIViewController {
 
-     @IBOutlet weak var colorView: UIView!
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    // MARK: - Override Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        @IBOutlet weak var redLabel: UILabel!
-        @IBOutlet weak var greenLabel: UILabel!
-        @IBOutlet weak var blueLabel: UILabel!
+        colorView.layer.cornerRadius = 15
         
-        @IBOutlet weak var redSlider: UISlider!
-        @IBOutlet weak var greenSlider: UISlider!
-        @IBOutlet weak var blueSlider: UISlider!
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            colorView.layer.cornerRadius = 15
-            
-            redSlider.tintColor = .red
-            greenSlider.tintColor = .green
-            
-            setColor()
-            setValue(for: redLabel, greenLabel, blueLabel)
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+    }
+    
+    // MARK: - IB Actions
+    
+    // Изменение цветов слайдерами
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        switch sender.tag {
+        case 0: redLabel.text = string(from: sender)
+        case 1: greenLabel.text = string(from: sender)
+        case 2: blueLabel.text = string(from: sender)
+        default: break
         }
         
-        // Изменение цветов слайдерами
-        @IBAction func rgbSlider(_ sender: UISlider) {
-            switch sender.tag {
-            case 0: redLabel.text = string(from: sender)
-            case 1: greenLabel.text = string(from: sender)
-            case 2: blueLabel.text = string(from: sender)
+        setColor()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabel.text = string(from: redSlider)
+            case 1: greenLabel.text = string(from: greenSlider)
+            case 2: blueLabel.text = string(from: blueSlider)
             default: break
             }
-            
-            setColor()
-        }
-        
-        private func setColor() {
-            colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                                green: CGFloat(greenSlider.value),
-                                                blue: CGFloat(blueSlider.value),
-                                                alpha: 1)
-        }
-        
-        private func setValue(for labels: UILabel...) {
-            labels.forEach { label in
-                switch label.tag {
-                case 0: redLabel.text = string(from: redSlider)
-                case 1: greenLabel.text = string(from: greenSlider)
-                case 2: blueLabel.text = string(from: blueSlider)
-                default: break
-                }
-            }
-        }
-        
-        private func string(from slider: UISlider) -> String {
-            String(format: "%.2f", slider.value)
         }
     }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
 
