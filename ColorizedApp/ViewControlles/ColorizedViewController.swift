d6bad8e74e2794c62972fdf7a175839fb3afa15e
Egor Ukolov <ukolovegor@yandex.ru>
2020-06-07 19:49:48 +0700

    Реализовано задание и передача значения с TextField в Sliders и Labels

--- ColorizedApp/ViewControlles/ColorizedViewController.swift
+++ ColorizedApp/ViewControlles/ColorizedViewController.swift
@@ -16,7 +16,7 @@ protocol ColorizedViewControllerDelegate {
     func saveColor(_ color: UIView)
 }
 
-class ColorizedViewController: UIViewController, CurrentColorProtocol {
+class ColorizedViewController: UIViewController, CurrentColorProtocol, UITextFieldDelegate {
 
     // MARK: - IB Outlets
     
@@ -47,16 +47,23 @@ class ColorizedViewController: UIViewController, CurrentColorProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.backgroundColor = secondTimeColorView.backgroundColor
-        
-        
         colorView.layer.cornerRadius = 15
         
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
         
         setColor()
+        
+        
         setValue(for: redLabel, greenLabel, blueLabel)
+        
+        setValueForTextField(for: redTextField, greenTextField, blueTextField)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+        
     }
     
     // MARK: - IB Actions
@@ -64,9 +71,42 @@ class ColorizedViewController: UIViewController, CurrentColorProtocol {
     // Изменение цветов слайдерами
     @IBAction func rgbSlider(_ sender: UISlider) {
         switch sender.tag {
-        case 0: redLabel.text = string(from: sender)
-        case 1: greenLabel.text = string(from: sender)
-        case 2: blueLabel.text = string(from: sender)
+            
+        case 0: redLabel.text = string(from: sender);
+                redTextField.text = string(from: sender)
+            
+        case 1: greenLabel.text = string(from: sender);
+                greenTextField.text = string(from: sender)
+            
+        case 2: blueLabel.text = string(from: sender);
+                blueTextField.text = string(from: sender)
+        
+        default: break
+        }
+        
+        setColor()
+    }
+    
+
+    // метод для передачи вносимых значений в слайдеры и лейблы
+    
+     func textFieldDidEndEditing(_ textField: UITextField) {
+      
+    }
+    
+    
+    @IBAction func valuesTextField(_ sender: UITextField) {
+        switch sender.tag {
+            
+        case 0: redLabel.text = sender.text;
+        redSlider.value = Float(sender.text!)!
+            
+        case 1: greenLabel.text = sender.text;
+        greenSlider.value = Float(sender.text!)!
+            
+        case 2: blueLabel.text = sender.text;
+        blueSlider.value = Float(sender.text!)!
+            
         default: break
         }
         
@@ -94,13 +134,28 @@ class ColorizedViewController: UIViewController, CurrentColorProtocol {
             case 0: redLabel.text = string(from: redSlider)
             case 1: greenLabel.text = string(from: greenSlider)
             case 2: blueLabel.text = string(from: blueSlider)
+            
             default: break
             }
         }
     }
     
+    private func setValueForTextField(for texts: UITextField...) {
+            texts.forEach { text in
+                switch text.tag {
+                case 0: redTextField.text = string(from: redSlider)
+                case 1: greenTextField.text = string(from: greenSlider)
+                case 2: blueTextField.text = string(from: blueSlider)
+
+                default: break
+                }
+            }
+        }
+        
+
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
+   
 }
 
